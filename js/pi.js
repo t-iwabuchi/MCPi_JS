cf7028587fe84b1f056a8596b7080d8f6aaf3c3f
programmingprivate <programmingprivate@gmail.com>
2017-06-28 19:06:26 +0900

    view canvas thinking design

--- js/pi.js
+++ js/pi.js
@@ -8,6 +8,8 @@ function draw() {
 		return false;
 	}
 	var ctx = canvas.getContext('2d');
+        ctx.fillStyle = 'rgb(255,255,255)';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 	ctx.beginPath();
 	ctx.arc(canvas.width/2, canvas.height/2, canvas.width/2, 0, Math.PI*2);
 	ctx.stroke();
